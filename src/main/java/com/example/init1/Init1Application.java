f57fee47153005efaeb475106b7439a36924c050
윤지섭 <jisub@bejewel.kr>
2022-05-16 20:43:49 +0900

    init commit

---
+++ src/main/java/com/example/init1/Init1Application.java
@@ -0,0 +1,13 @@
+package com.example.init1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Init1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Init1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/init1/Init1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.init1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Init1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
